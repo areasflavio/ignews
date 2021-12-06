d07e00b013e2aea96e82909656bd36d5c7bd0744
Flávio Arêas <vflavio9@gmail.com>
2021-12-06 20:17:41 -0300

    feat: connect to Stripe API

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-
 import styles from './styles.module.scss';
 
-const SubscribeButton: React.FC = () => {
+interface SubscribeButtonProps {
+  priceId: string;
+}
+
+const SubscribeButton: React.FC<SubscribeButtonProps> = ({ priceId }) => {
   return (
     <button type="button" className={styles.subscribeButton}>
       Subscribe now
