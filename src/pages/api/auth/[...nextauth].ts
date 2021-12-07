5ce226e2deb61cc23586d90c995acf2c05a64347
Flávio Arêas <vflavio9@gmail.com>
2021-12-07 19:43:31 -0300

    feat: connect to FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import { query as q } from 'faunadb';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -15,4 +18,19 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  jwt: { secret: process.env.SIGNING_KEY },
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      try {
+        await fauna.query(
+          q.Create(q.Collection('users'), { data: { email: user.email } })
+        );
+
+        return true;
+      } catch (err: any) {
+        console.error(err.message);
+        return false;
+      }
+    },
+  },
 });
