7c51966424bc24dab0b11da2ae476b221cb26308
Flávio Arêas <vflavio9@gmail.com>
2021-12-09 18:21:37 -0300

    feat: show post page only if subscribed

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -19,6 +19,33 @@ export default NextAuth({
     // ...add more providers here
   ],
   callbacks: {
+    async session({ session, user, token }) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  'ref',
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(q.Index('subscription_by_status'), 'active'),
+            ])
+          )
+        );
+
+        return { ...session, activeSubscription: userActiveSubscription };
+      } catch (err) {
+        return { ...session, activeSubscription: null };
+      }
+    },
     async signIn({ user, account, profile, email, credentials }) {
       try {
         await fauna.query(
