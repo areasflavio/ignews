957d2c4152239e2e58600faf3653cb1a78a43f9a
Flávio Arêas <vflavio9@gmail.com>
2022-02-28 17:16:44 -0300

    fix: set next auth session access token

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -21,6 +21,8 @@ export default NextAuth({
   ],
   callbacks: {
     async session({ session, user, token }) {
+      session.accessToken = token.accessToken;
+
       try {
         const userActiveSubscription = await fauna.query(
           q.Get(
@@ -67,16 +69,12 @@ export default NextAuth({
         return false;
       }
     },
-  },
-  cookies: {
-    sessionToken: {
-      name: `__Secure-next-auth.session-token`,
-      options: {
-        httpOnly: true,
-        sameSite: 'lax',
-        path: '/',
-        secure: true,
-      },
+    async jwt({ token, account }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
     },
   },
 });
